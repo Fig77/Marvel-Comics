b64a066c7e770b44cee4d55838caf608612c6b9d
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-08-23 22:47:35 -0300

    Added api with public key. Configured api refers. Added a default with a 10 limit.

--- src/API/apidata.js
+++ src/API/apidata.js
@@ -1,3 +1,15 @@
 const apidata = (() => {
+  const KEY_API = `90c704a85bddb1baa24fcd3058cc5d3c`;
+
+  const fetchData = async () => {
+    const endpoint = `https://gateway.marvel.com/v1/public/comics?limit=10&apikey=${KEY_API}`;
+    const data = await (await fetch(endpoint)).json();
+    console.log(data);
+  };
   
+  return {
+    fetchData
+  }
 })();
+
+export default apidata;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import api from './API/apidata';
+
+api.fetchData();
 
 ReactDOM.render(
   <React.StrictMode>
