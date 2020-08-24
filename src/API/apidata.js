9b47e30847a86e11d5a366c2200ddea06c5473f0
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-08-23 23:13:15 -0300

    eslint fixes

--- src/API/apidata.js
+++ src/API/apidata.js
@@ -1,15 +1,15 @@
 const apidata = (() => {
-  const KEY_API = `90c704a85bddb1baa24fcd3058cc5d3c`;
+  const KEY_API = '90c704a85bddb1baa24fcd3058cc5d3c';
 
   const fetchData = async () => {
     const endpoint = `https://gateway.marvel.com/v1/public/comics?limit=10&apikey=${KEY_API}`;
     const data = await (await fetch(endpoint)).json();
-    console.log(data);
+    return data;
   };
-  
+
   return {
-    fetchData
-  }
+    fetchData,
+  };
 })();
 
 export default apidata;
