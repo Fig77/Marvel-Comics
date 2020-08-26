690c9f458aae1b052e158efd8264a6ff866ba9b5
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-08-26 01:49:51 -0300

    syntax error

--- src/component/ComicDetail.jsx
+++ src/component/ComicDetail.jsx
@@ -2,6 +2,34 @@ import React from 'react';
 
 const ComicDetail = (props) => {
   
+  const {
+    id,
+    image,
+    title,
+    description,
+    thubmnail,
+    characters,
+    issuenumber
+  } = props;
+
+  return (
+  <section key={id}>
+    <div>
+      <h2>{ title }</h2>
+      <div>
+        <p> {description} </p>
+        <div>
+          <span> {issuenumber} </span>
+          <ul> { characters } </ul>
+        </div>
+      </div>
+    </div>
+    <div>
+      <img src={thumbnail.path.concat("/portrait_xlarge.")}/>
+    </div>
+  </section>
+  );
+
 };
 
 export default ComicDetail;
