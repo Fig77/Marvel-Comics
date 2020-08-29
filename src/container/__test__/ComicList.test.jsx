d61f8af1afefa62ef03d465a7dbdbed27b1a485c
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-08-29 00:11:09 -0300

    Testing routing on ComcList working fine

--- src/container/__test__/ComicList.test.jsx
+++ src/container/__test__/ComicList.test.jsx
@@ -1,63 +1,64 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import {Provider, connect} from 'react-redux'
-import {render as rtlRender, screen, fireEvent} from '@testing-library/react'
+import {render, screen, fireEvent} from '@testing-library/react'
 import Comic from '../../component/Comic'
-import {Link, Route, Router, Switch} from 'react-router-dom'
+import {Link, Route, Router, Switch, BrowserRouter} from 'react-router-dom'
 import utils from '../../testUtils/testUtils';
-
+import { within } from '@testing-library/dom'
+import {createMemoryHistory} from 'history'
+import TestUtils from 'react-dom';
 import ComicList from '../ComicList';
 
-
-const getData = (())
-   const data = [
-      {
-        "id": 58587,
-        "title": "The Amazing Spider-Man (2015) #21 (Rivera Variant)",
-        "issueNumber": 21,
-        "description": null,
-        "format": "Comic",
-        "urls": [
-          {
-            "type": "detail",
-            "url": "http://marvel.com/comics/issue/58587/the_amazing_spider-man_2015_21_rivera_variant/rivera_variant?utm_campaign=apiRef&utm_source=90c704a85bddb1baa24fcd3058cc5d3c"
+//Mocking data & Functions
+  const data = [ 
+    {
+      "id": 58587,
+      "title": "The Amazing Spider-Man (2015) #21 (Rivera Variant)",
+      "issueNumber": 21,
+      "description": null,
+      "format": "Comic",
+      "urls": [
+        {
+          "type": "detail",
+          "url": "http://marvel.com/comics/issue/58587/the_amazing_spider-man_2015_21_rivera_variant/rivera_variant?utm_campaign=apiRef&utm_source=90c704a85bddb1baa24fcd3058cc5d3c"
           }
         ],
-        "thumbnail": {
-          "path": "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available",
-          "extension": "jpg"
-        },
-        "images": []
+      "thumbnail": {
+        "path": "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available",
+        "extension": "jpg"
+      },
+      "images": []
     },
-     {
-        "id": 40921,
-        "title": "Ghost Rider: Official Index to the Marvel Universe GN-TPB (Graphic Novel)",
-        "issueNumber": 1,
-        "description": "The complete history of the Spirit of Vengeance from\r\nhis earliest appearances to the present day! This\r\nfact-packed volume chronicles every character, team,\r\nplace and piece of equipment &mdash; and provides vital\r\ninformation about all things Ghost Rider! Collecting\r\nthe Ghost Rider material from WOLVERINE, PUNISHER\r\n& GHOST RIDER: OFFICIAL INDEX TO THE MARVEL\r\nUNIVERSE #1-6.\r\n152 PGS./Rated T+ &#133;$19.99",
-        "format": "Graphic Novel",
-        "urls": [
-          {
-            "type": "detail",
-            "url": "http://marvel.com/comics/collection/40921/ghost_rider_official_index_to_the_marvel_universe_gn-tpb_graphic_novel?utm_campaign=apiRef&utm_source=90c704a85bddb1baa24fcd3058cc5d3c"
+    {
+      "id": 40921,
+      "title": "Ghost Rider: Official Index to the Marvel Universe GN-TPB (Graphic Novel)",
+      "issueNumber": 1,
+      "description": "The complete history of the Spirit of Vengeance from\r\nhis earliest appearances to the present day! This\r\nfact-packed volume chronicles every character, team,\r\nplace and piece of equipment &mdash; and provides vital\r\ninformation about all things Ghost Rider! Collecting\r\nthe Ghost Rider material from WOLVERINE, PUNISHER\r\n& GHOST RIDER: OFFICIAL INDEX TO THE MARVEL\r\nUNIVERSE #1-6.\r\n152 PGS./Rated T+ &#133;$19.99",
+      "format": "Graphic Novel",
+      "urls": [
+        {
+          "type": "detail",
+          "url": "http://marvel.com/comics/collection/40921/ghost_rider_official_index_to_the_marvel_universe_gn-tpb_graphic_novel?utm_campaign=apiRef&utm_source=90c704a85bddb1baa24fcd3058cc5d3c"
           }
         ],
-        "thumbnail": {
+      "thumbnail": {
+        "path": "http://i.annihil.us/u/prod/marvel/i/mg/b/a0/4ec2b1272cee0",
+        "extension": "jpg"
+      },
+      "images": [
+        {
           "path": "http://i.annihil.us/u/prod/marvel/i/mg/b/a0/4ec2b1272cee0",
           "extension": "jpg"
-        },
-        "images": [
-          {
-            "path": "http://i.annihil.us/u/prod/marvel/i/mg/b/a0/4ec2b1272cee0",
-            "extension": "jpg"
           }
         ],
       }
  ];
-  
+
 const noData = [];
 
 const setMock = (i, data) => {
-  return (<Comic key={i} item={data[i]} id={i} /> );
+  return (<Comic key={i} item={data[i]} id={i} /> ); // Elementen functionality and data is tested here as well.
 };
 
 function comicMock() {
@@ -74,7 +75,7 @@ test('Show Loading message if loading is true', () => {
   const message = 'Loading! wait a sec...';
   utils.render(<ComicList filter={(event)=>{}} loading={true} dataSet={true} comicMap = {comicMock}/>);
   expect(screen.getByText(message)).toBeDefined()
-})
+});
 
 test('Shows comics expected from mock', () => {
   const title_1 = 'Ghost Rider: Official Index to the Marvel Universe GN-TPB (Graphic Novel)';
@@ -85,9 +86,11 @@ test('Shows comics expected from mock', () => {
   expect(screen.getByText(title_2)).toBeDefined();
 });
 
-test('Shows no comics message if no comic have been found', () => {
-  const errmessage = 'No comics to be found!';
-  
-  utils.render(<ComicList filter={(event)=>{}} loading={false} dataSet={true} comicMap={comicMock}/>);
-  expect(screen.getByText(errmessage)).toBeDefined();
+test('Proper routes for 0..1 elements should be in place ', () => {
+  const {container, getByTestId} = utils.browserR(<ComicList filter={(event)=>{}} loading={false} dataSet={false} comicMap={comicMock}/>);
+  let route_0 = "/comic/0";
+  let route_1 = "/comic/1";
+  let routeMatcher = [new RegExp(route_0.replace(/:[^\s/]+/g, '([\\w-]+)')), new RegExp(route_1.replace(/:[^\s/]+/g, '([\\w-]+)'))]
+  expect(screen.getByTestId('0').href).toMatch(routeMatcher[0]);
+  expect(screen.getByTestId('1').href).toMatch(routeMatcher[1]);
 });
--- src/testUtils/testUtils.js
+++ src/testUtils/testUtils.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {render as rtlRender} from '@testing-library/react'
 import rootReducer from '../reducer/index';
-import {Link, Route, Router, Switch} from 'react-router-dom'
+import {Link, Route, Router, Switch, BrowserRouter} from 'react-router-dom'
 import {createMemoryHistory} from 'history'
 import {createStore} from 'redux'
 import {Provider, connect} from 'react-redux'
@@ -34,9 +34,37 @@ const utils = (() => {
       ...renderOptions
     })
   }
+  
+function browserR(
+    ui, {
+      route = '/',
+      history = createMemoryHistory({
+        initialEntries: [route]
+      })
+    } = {}, {
+      store = rootReducer.store,
+      ...renderOptions
+    } = {},
+  ) {
+    function Wrapper({
+      children
+    }) {
+      return <Router history = {
+        history
+      } > <BrowserRouter>< Provider store = {
+        store
+      } > {
+        children 
+      } < /Provider></BrowserRouter></Router >
+    }
+    return rtlRender(ui, {
+      wrapper: Wrapper,
+      ...renderOptions
+    })
+  }
 
   return {
-    render
+    render, browserR
   }
 })();
 
