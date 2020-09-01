4f8ef37c46633799907680842280380547e628d5
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-01 17:50:44 -0300

    Added particular test and matcher for the api

---
+++ src/API/__test__/apidata.test.js
@@ -0,0 +1,35 @@
+import api from '../apidata';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect'
+
+expect.extend({
+  async toFetchDataBulk() {
+    const result = await api.fetchData();
+    let i = 0;
+    while (i < result.length) {
+      if (result[i].status !== 'Ok') {
+        return {
+          message: () =>
+            `expected status of 4 elements to be Ok, but got ${result[i].status}`,
+          pass: false,
+        };
+      }
+      if (result[i].data.results === undefined) {
+        return {
+          message: () =>
+            `expected data results not to be undefined`,
+          pass: false,
+        };
+
+      }
+      i += 1;
+    }
+  }
+});
+
+describe('Call to API will return 200 status and 20 elements that will have a data attribute', () => {
+  test('api to api const url should be able to GET', async () => {
+    const result = await api.fetchData();
+    expect(result).toFetchDataBulk();
+  });
+});
