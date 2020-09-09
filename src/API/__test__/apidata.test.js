7e798135b36d0abe54660fbae80da6c6bd1f38fc
Fig77 <55416179+Fig77@users.noreply.github.com>
2020-09-09 13:35:17 -0300

    Fixed custom matcher and extra call to the api

--- src/API/__test__/apidata.test.js
+++ src/API/__test__/apidata.test.js
@@ -3,30 +3,30 @@ import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 
 expect.extend({
-  async toFetchDataBulk() {
-    const result = await api.fetchData();
+  async toFetchDataBulk(result) {
     let i = 0;
     while (i < result.length) {
       if (result[i].status !== 'Ok') {
         return {
-          message: () => `expected status of 4 elements to be Ok, but got ${result[i].status}`,
+          message: `expected status of 4 elements to be Ok, but got ${result[i].status}`,
           pass: false,
         };
       }
       if (result[i].data.results === undefined) {
         return {
-          message: () => 'expected data results not to be undefined',
+          message: 'expected data results not to be undefined',
           pass: false,
         };
       }
       i += 1;
     }
+   return {pass: true}
   },
 });
 
 describe('Call to API will return 200 status and 20 elements that will have a data attribute', () => {
   test('api to api const url should be able to GET', async () => {
     const result = await api.fetchData();
-    expect(result).toFetchDataBulk();
+    await expect(result).toFetchDataBulk();
   });
 });
